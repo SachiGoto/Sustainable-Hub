dc64a268af48c43d8c3889095e11471959daaede
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-04-10 23:26:15 -0700

    worked on home page responsiveness

--- front-end/100-hours-project-front-end/src/pages/Home.js
+++ front-end/100-hours-project-front-end/src/pages/Home.js
@@ -8,16 +8,16 @@ const Home = () => {
  
  
   return (
-    <>
+    < >
 
 
-    <div className="home">
-      <div className="heroImage-container">
-        <div className="heroContent">
-          <p className="text-base">
+    <div>
+      <div className="flex flex-col items-center justify-center mt-20 ">
+      <div className="sm:w-80 sm:relative md:absolute md:top-1/2 md:left-1/4 md:-translate-x-2/4 md:-translate-y-1/2 md:w-90 lg:translate-x-1/4 lg:left-1/4 lg:w-90 z-1">
+          <h3 className="text-base text-l">
           Sustainable Hub is designed to help people live a more sustainable lifestyle by providing practical suggestions for sustainable activities in their day-to-day. Providing tips on sustainable living accessible to everyone. Click the category button to see a list of local companies in Vanocuver,Canada that offer sustainable products and services! 
           
-          </p>
+          </h3>
 
           <button
             className="btn "
@@ -26,7 +26,7 @@ const Home = () => {
             <Link to="main2"> Categories </Link>
           </button>
         </div>
-        <div className="heroImage">
+      <div className="sm:relative sm:w-full md:absolute md:w-80% md:top-1/2 md:right-0 md:-translate-y-2/4 lg:-translate-x-1/4 lg:right1/4 lg:w-3/4 max-w-screen-md -z-1">
           <img src={logo} alt="hero" />
         </div>
       </div>
