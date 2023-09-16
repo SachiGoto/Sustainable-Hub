5f1b369d574094a411b5b822021222d2ff45f0bb
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-09-15 23:52:02 -0700

    worked on timeline

--- front-end/100-hours-project-front-end/src/components/SubTimeline.js
+++ front-end/100-hours-project-front-end/src/components/SubTimeline.js
@@ -4,14 +4,16 @@ import { useState, useRef, useEffect } from "react";
 const SubTimeline = ({ subtimeline, animateElement }) => {
   const ref = useRef();
   const [iconImage, setIconImage] = useState(subtimeline[1].business.img);
+  const [description, setDescription] = useState(subtimeline[1].business.description);
 
   useEffect(() => {
     animateElement(ref, {});
   }, [animateElement]);
 
-  function changeImage(newIconImage) {
+  function update(newIconImage, newDescription) {
     console.log("clicked", newIconImage);
     setIconImage(newIconImage);
+    setDescription(newDescription);
   }
   const eachSubTimeLine = subtimeline.map((eachSubTimeline, i) => {
     return (
@@ -19,7 +21,7 @@ const SubTimeline = ({ subtimeline, animateElement }) => {
         <div key={i}>
           <p
             className="px-3 z-100 text-sm md:text-base"
-            onClick={() => changeImage(eachSubTimeline.business.img)}
+            onClick={() => update(eachSubTimeline.business.img, eachSubTimeline.business.description)}
           >
             {eachSubTimeline.name}
           </p>
@@ -33,13 +35,17 @@ const SubTimeline = ({ subtimeline, animateElement }) => {
   });
 
   return (
-
-      <div className="my-5 flex flex-col " ref={ref}>
-        <div className="flex">{eachSubTimeLine}</div>
-
-        <img src={iconImage} alt="Icon" className="w-[60%]" />
+    <div className="my-5 flex flex-col " ref={ref}>
+      <div className="flex mt-5">{eachSubTimeLine}</div>
+      <div className="flex w-[80%] max-w-[600px] mt-5 justify-center items-center h-[300px;] ">
+        <div className="w-[50%]">
+          <p class="text-xs">{description}</p>
+        </div>
+        <div className="w-[50%] h-[200px] ml-5 ">
+          <img src={iconImage} alt="Icon" className="object-cover w-[100%] h-[100%]" />
+        </div>
       </div>
-
+    </div>
   );
 };
 
